11ca82e7ab52eb04993190ebecbeefc243099e2a
lgy0404 <2528379691@qq.com>
2024-05-10 13:42:04 +0000

    api key测试结束

--- testmykey/openai/myopenai.py
+++ testmykey/openai/myopenai.py
@@ -1,35 +1,35 @@
-##-----------------OPRNAI_ZL-----------------##
-# # python3
-# # please install OpenAI SDK first: `pip3 install openai`
-# from openai import OpenAI
-
-# client = OpenAI(api_key="<sk-1WSYBg4Sg8AwZu0Zr1mu1eTZmnCsFDuoq4sSHZMgEZbEt5iE>", base_url="https://api.chatanywhere.tech")
-
-# response = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant"},
-#         {"role": "user", "content": "Hello"},
-#     ]
-# )
-
-# print(response.choices[0].message.content)
-
-
-##-----------------OPRNAI_TB-----------------##
+#-----------------OPRNAI_ZL-----------------##
 # python3
 # please install OpenAI SDK first: `pip3 install openai`
 from openai import OpenAI
 
-client = OpenAI(api_key="<sk-iV0XN5ue0PHE6mhR612491C1F3Cf4562B35dB291C5Dd329a>", base_url="https://api.pumpkinaigc.online")
+client = OpenAI(api_key="<sk-zKs3eVUnmpT281l1bs6CdFPUaabv0ocMSonmH9FekGftF5hZ>", base_url="https://api.chatanywhere.tech")
 
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "你是谁"},
+        {"role": "user", "content": "Hello"},
     ]
 )
-print(response)
 
-# print(response.choices[0].message.content)
+print(response.choices[0].message.content)
+
+
+# ##-----------------OPRNAI_TB-----------------##
+# # python3
+# # please install OpenAI SDK first: `pip3 install openai`
+# from openai import OpenAI
+
+# client = OpenAI(api_key="<sk-iV0XN5ue0PHE6mhR612491C1F3Cf4562B35dB291C5Dd329a>", base_url="https://api.pumpkinaigc.online")
+
+# response = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant"},
+#         {"role": "user", "content": "你是谁"},
+#     ]
+# )
+# print(response)
+
+# # print(response.choices[0].message.content)
