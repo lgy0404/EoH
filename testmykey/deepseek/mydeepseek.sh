f14d3ee28513b32e2f0892a0487b609588a05919
lgy0404 <2528379691@qq.com>
2024-05-10 13:48:30 +0000

    修改deepseek脚本

--- testmykey/deepseek/mydeepseek.sh
+++ testmykey/deepseek/mydeepseek.sh
@@ -1,7 +1,7 @@
 # bash
 curl https://api.deepseek.com/chat/completions \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $sk-291d3d088eac47398cfa901ad0affd07" \
+  -H "Authorization: Bearer sk-291d3d088eac47398cfa901ad0affd07" \
   -d '{
         "model": "deepseek-chat",
         "messages": [
