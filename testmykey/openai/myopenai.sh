142afdc527111b33b16842098733890f77e58a17
lgy0404 <2528379691@qq.com>
2024-05-10 13:34:30 +0000

    使用脚本文件测试chatanywhere现有key的有效性

--- testmykey/openai/myopenai.sh
+++ testmykey/openai/myopenai.sh
@@ -1,11 +1,49 @@
-# bash
-curl https://api.deepseek.com/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $DEEPSEEK_API_KEY" \
+# # bash
+# curl https://api.deepseek.com/chat/completions \
+#   -H "Content-Type: application/json" \
+#   -H "Authorization: Bearer $DEEPSEEK_API_KEY" \
+#   -d '{
+#         "model": "deepseek-chat",
+#         "messages": [
+#           {"role": "system", "content": "You are a helpful assistant."},
+#           {"role": "user", "content": "Hello!"}
+#         ]
+#       }'
+
+
+# curl https://api.chatanywhere.tech/v1/chat/completions \
+#   -H 'Content-Type: application/json' \
+#   -H 'Authorization: Bearer sk-1WSYBg4Sg8AwZu0Zr1mu1eTZmnCsFDuoq4sSHZMgEZbEt5iE' \
+#   -d '{
+#         "model": "gpt-3.5-turbo",
+#         "messages": [{"role": "user", "content": "Say this is a test!"}],
+#         "temperature": 0.7
+#         }'
+
+curl https://api.chatanywhere.tech/v1/chat/completions \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer sk-zKs3eVUnmpT281l1bs6CdFPUaabv0ocMSonmH9FekGftF5hZ' \
   -d '{
-        "model": "deepseek-chat",
-        "messages": [
-          {"role": "system", "content": "You are a helpful assistant."},
-          {"role": "user", "content": "Hello!"}
-        ]
-      }'
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Say this is a test!"}],
+        "temperature": 0.7
+        }'
+
+
+# curl https://api.chatanywhere.tech/v1/chat/completions \
+#   -H 'Content-Type: application/json' \
+#   -H 'Authorization: Bearer  sk-6ien0ZfxkXYW4a4kKI4rkhmbWyQD3b3SXeZmo6gfwuf1VN4U' \
+#   -d '{
+#         "model": "gpt-3.5-turbo",
+#         "messages": [{"role": "user", "content": "Say this is a test!"}],
+#         "temperature": 0.7
+#         }'
+
+# curl https://api.chatanywhere.tech/v1/chat/completions \
+#   -H 'Content-Type: application/json' \
+#   -H 'Authorization: Bearer  sk-MF9UsaCNeOhVCdLuw2QGHgj3wdloSr507TQLEU34HtRbgYHM' \
+#   -d '{
+#         "model": "gpt-3.5-turbo",
+#         "messages": [{"role": "user", "content": "Say this is a test!"}],
+#         "temperature": 0.7
+#         }'
