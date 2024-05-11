527095f23e17afcae18bdcb607ea7935817ea329
lgy0404 <2528379691@qq.com>
2024-05-11 14:04:15 +0800

    add deepseek key to bp online

--- examples/bp_online/runEoH.py
+++ examples/bp_online/runEoH.py
@@ -7,9 +7,9 @@ paras = Paras()
 # Set parameters #
 paras.set_paras(method = "eoh",    # ['ael','eoh']
                 problem = "bp_online", #['tsp_construct','bp_online']
-                llm_api_endpoint = "XXX", # set your LLM endpoint
-                llm_api_key = "XXX",   # set your key
-                llm_model = "gpt-3.5-turbo",
+                llm_api_endpoint = "api.deepseek.com", # set your LLM endpoint
+                llm_api_key = "sk-291d3d088eac47398cfa901ad0affd07",   # set your key
+                llm_model = "deepseek-chat",
                 ec_pop_size = 4, # number of samples in each population
                 ec_n_pop = 4,  # number of populations
                 exp_n_proc = 4,  # multi-core parallel
