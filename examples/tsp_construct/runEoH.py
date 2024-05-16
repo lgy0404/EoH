d280d3a4c0e9f89affba7406369f48f349d035d0
lgy0404 <2528379691@qq.com>
2024-05-16 18:42:14 +0800

    切换zhenli apikey

--- examples/tsp_construct/runEoH.py
+++ examples/tsp_construct/runEoH.py
@@ -23,31 +23,7 @@
 # evolution.run()
 
 
-##-----------------OPRNAI_ZL-----------------##
-# from eoh import eoh
-# from eoh.utils.getParas import Paras
-
-# # Parameter initilization #
-# paras = Paras() 
-
-# # Set parameters #
-# paras.set_paras(method = "eoh",    # ['ael','eoh']
-#                 problem = "tsp_construct", #['tsp_construct','bp_online']
-#                 llm_api_endpoint = "api.chatanywhere.tech", # set your LLM endpoint
-#                 llm_api_key = "sk-zKs3eVUnmpT281l1bs6CdFPUaabv0ocMSonmH9FekGftF5hZ",   # set your key
-#                 llm_model = "gpt-3.5-turbo",
-#                 ec_pop_size = 4, # number of samples in each population
-#                 ec_n_pop = 4,  # number of populations
-#                 exp_n_proc = 4,  # multi-core parallel
-#                 exp_debug_mode = False)
-
-# # initilization
-# evolution = eoh.EVOL(paras)
-
-# # run 
-# evolution.run()
-
-#-----------------OPRNAI_TB-----------------##
+#-----------------OPRNAI_ZL-----------------##
 from eoh import eoh
 from eoh.utils.getParas import Paras
 
@@ -57,9 +33,9 @@ paras = Paras()
 # Set parameters #
 paras.set_paras(method = "eoh",    # ['ael','eoh']
                 problem = "tsp_construct", #['tsp_construct','bp_online']
-                llm_api_endpoint = "api.deepseek.com", # set your LLM endpoint
-                llm_api_key = "sk-291d3d088eac47398cfa901ad0affd07",   # set your key
-                llm_model = "deepseek-chat",
+                llm_api_endpoint = "api.chatanywhere.tech", # set your LLM endpoint
+                llm_api_key = "sk-zKs3eVUnmpT281l1bs6CdFPUaabv0ocMSonmH9FekGftF5hZ",   # set your key
+                llm_model = "gpt-3.5-turbo",
                 ec_pop_size = 4, # number of samples in each population
                 ec_n_pop = 4,  # number of populations
                 exp_n_proc = 4,  # multi-core parallel
@@ -70,6 +46,30 @@ evolution = eoh.EVOL(paras)
 
 # run 
 evolution.run()
+
+# #-----------------deepseek-----------------##
+# from eoh import eoh
+# from eoh.utils.getParas import Paras
+
+# # Parameter initilization #
+# paras = Paras() 
+
+# # Set parameters #
+# paras.set_paras(method = "eoh",    # ['ael','eoh']
+#                 problem = "tsp_construct", #['tsp_construct','bp_online']
+#                 llm_api_endpoint = "api.deepseek.com", # set your LLM endpoint
+#                 llm_api_key = "sk-291d3d088eac47398cfa901ad0affd07",   # set your key
+#                 llm_model = "deepseek-chat",
+#                 ec_pop_size = 4, # number of samples in each population
+#                 ec_n_pop = 4,  # number of populations
+#                 exp_n_proc = 4,  # multi-core parallel
+#                 exp_debug_mode = False)
+
+# # initilization
+# evolution = eoh.EVOL(paras)
+
+# # run 
+# evolution.run()
 
 
 ##-----------------ORIGIN-----------------##
